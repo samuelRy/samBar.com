eb4b60b5de01bcb2b37ded93a1930b0b91817340
Ryan Barkah <elijahsamuelbarkah@gmail.com>
2026-02-04 03:50:19 +0100

    env issue again

--- src/config_db.php
+++ src/config_db.php
@@ -2,11 +2,11 @@
 // config.php - SAFE to commit
 return [
     'db' => [
-        'host' => $_ENV('DB_HOST') ?: 'localhost',
-        'port' => $_ENV('DB_PORT') ?: 3306,
-        'user' => $_ENV('DB_USER') ?: 'root',
-        'pass' => $_ENV('DB_PASS') ?: '',
-        'name' => $_ENV('DB_NAME') ?: 'my_db'
+        'host' => $_ENV('DB_HOST'),
+        'port' => $_ENV('DB_PORT'),
+        'user' => $_ENV('DB_USER'),
+        'pass' => $_ENV('DB_PASS'),
+        'name' => $_ENV('DB_NAME')
     ]
 ];
 ?>
