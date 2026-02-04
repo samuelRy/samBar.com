74fed0dd1e30b305944281aebda410f21b8d4447
Ryan Barkah <elijahsamuelbarkah@gmail.com>
2026-02-04 04:23:35 +0100

    env again

--- src/config_db.php
+++ src/config_db.php
@@ -1,10 +1,15 @@
 <?php
 // config.php - SAFE to commit
+
 $host = $_SERVER['DB_HOST'] ?? 'localhost';
 $port = $_SERVER['DB_PORT'] ?? 3306;
 $user = $_SERVER['DB_USER'] ?? 'root';
 $pass = $_SERVER['DB_PASS'] ?? '';
 $name = $_SERVER['DB_NAME'] ?? 'defaultdb';
+echo $_SERVER['DB_NAME'];
+echo $_ENV['DB_NAME'];
+echo getenv('DB_NAME');
+
 return [
     'db' => [
         'host' => $host,
