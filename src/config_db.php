440aa006c5cf231026d6cc416d61938193028d0d
Ryan Barkah <elijahsamuelbarkah@gmail.com>
2026-02-04 04:35:10 +0100

    env fix

--- src/config_db.php
+++ src/config_db.php
@@ -1,15 +1,10 @@
 <?php
 // config.php - SAFE to commit
-
-$host = $_SERVER['DB_HOST'] ?? 'localhost';
-$port = $_SERVER['DB_PORT'] ?? 3306;
-$user = $_SERVER['DB_USER'] ?? 'root';
-$pass = $_SERVER['DB_PASS'] ?? '';
-$name = $_SERVER['DB_NAME'] ?? 'defaultdb';
-echo $_SERVER['DB_NAME'];
-echo $_ENV['DB_NAME'];
-echo getenv('DB_NAME');
-
+$host = $_ENV['DB_HOST'] ?? 'localhost';
+$port = $_ENV['DB_PORT'] ?? 3306;
+$user = $_ENV['DB_USER'] ?? 'root';
+$pass = $_ENV['DB_PASS'] ?? '';
+$name = $_ENV['DB_NAME'] ?? 'defaultdb';
 return [
     'db' => [
         'host' => $host,
