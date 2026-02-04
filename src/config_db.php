1b64fb45ef16049129ae099b36ea08e629739dc0
Ryan Barkah <elijahsamuelbarkah@gmail.com>
2026-02-04 03:56:31 +0100

    still env issue

--- src/config_db.php
+++ src/config_db.php
@@ -1,12 +1,17 @@
 <?php
 // config.php - SAFE to commit
+$host = $_ENV['DB_HOST'] ?? 'localhost';
+$port = $_ENV['DB_PORT'] ?? 3306;
+$user = $_ENV['DB_USER'] ?? 'root';
+$pass = $_ENV['DB_PASS'] ?? '';
+$name = $_ENV['DB_NAME'] ?? 'defaultdb';
 return [
     'db' => [
-        'host' => $_ENV('DB_HOST'),
-        'port' => $_ENV('DB_PORT'),
-        'user' => $_ENV('DB_USER'),
-        'pass' => $_ENV('DB_PASS'),
-        'name' => $_ENV('DB_NAME')
+        'host' => $host,
+        'port' => $port,
+        'user' => $user,
+        'pass' => $pass,
+        'name' => $name
     ]
 ];
 ?>
